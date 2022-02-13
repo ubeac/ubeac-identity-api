6b5123d127ef36b87cf7b700cf24d7729605b913
Hesam Asnaashari <ilhesam@outlook.com>
2022-02-13 15:09:59 +0330

    Initalize Program.cs file

--- src/Example/Program.cs
+++ src/Example/Program.cs
@@ -1,11 +1,49 @@
+using Example;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using uBeac.Repositories.MongoDB;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddHttpContextAccessor();
 builder.Services.AddControllers();
+
+// Adding json config files
+builder.Configuration.AddJsonConfig(builder.Environment);
+
+// Adding mongodb
+builder.Services.AddMongo<MongoDBContext>("DefaultConnection");
+
+// Adding repositories
+builder.Services.AddMongoDBUserRepository<MongoDBContext, CustomUser>();
+builder.Services.AddMongoDBUserTokenRepository<MongoDBContext>();
+builder.Services.AddMongoDBRoleRepository<MongoDBContext, CustomRole>();
+builder.Services.AddMongoDBUnitRepository<MongoDBContext, CustomUnit>();
+builder.Services.AddMongoDBUnitTypeRepository<MongoDBContext, CustomUnitType>();
+builder.Services.AddMongoDBUnitRoleRepository<MongoDBContext, CustomUnitRole>();
+
+// Adding services
+builder.Services.AddUserService<CustomUserService, CustomUser>();
+builder.Services.AddRoleService<CustomRoleService, CustomRole>();
+builder.Services.AddUserRoleService<CustomUserRoleService, CustomUser>();
+builder.Services.AddUnitService<CustomUnitService, CustomUnit>();
+builder.Services.AddUnitTypeService<CustomUnitTypeService, CustomUnitType>();
+builder.Services.AddUnitRoleService<CustomUnitRoleService, CustomUnitRole>();
+
+// Adding identity core
+builder.Services
+    .AddIdentityUser<CustomUser>()
+    .AddIdentityRole<CustomRole>()
+    .AddIdentityUnit<CustomUnit>()
+    .AddIdentityUnitType<CustomUnitType>();
+
+// Adding jwt authentication
+builder.Services.AddJwtAuthentication(builder.Configuration.GetInstance<JwtOptions>("Jwt"));
 
 var app = builder.Build();
 app.UseHttpsRedirection();
+app.UseDefaultFiles();
+app.UseStaticFiles();
 app.UseAuthorization();
 app.MapControllers();
 app.Run();
