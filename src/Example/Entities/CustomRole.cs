bf53931fe7e9d8ca4c7e223872580acbadef2234
Hesam Asnaashari <ilhesam@outlook.com>
2022-02-13 14:34:20 +0330

    Add custom entities

---
+++ src/Example/Entities/CustomRole.cs
@@ -0,0 +1,14 @@
+﻿namespace Example;
+
+// Inherits from Role
+public class CustomRole : Role
+{
+    public CustomRole() { }
+    public CustomRole(string name) : base(name) { }
+
+    // You can add your custom properties:
+    // public string YourCustomProperty { get; set; }
+
+    // Also, Override base properties:
+    // public override Guid Id { get; set; }
+}
---
+++ src/Example/Entities/CustomUnit.cs
@@ -0,0 +1,11 @@
+﻿namespace Example;
+
+// Inherits from Unit
+public class CustomUnit : Unit
+{
+    // You can add your custom properties:
+    // public string YourCustomProperty { get; set; }
+
+    // Also, Override base properties:
+    // public override Guid Id { get; set; }
+}
