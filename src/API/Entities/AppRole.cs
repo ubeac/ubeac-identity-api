94b3835ddb90859a48dce558b27dc45652f84a9e
Hesam Asnaashari <46031836+ilhesam@users.noreply.github.com>
2022-04-16 03:11:45 +0430

    Inherit from IAuditEntity

--- src/API/Entities/AppRole.cs
+++ src/API/Entities/AppRole.cs
@@ -1,7 +1,9 @@
-﻿namespace API;
+﻿using System;
+
+namespace API;
 
 // Inherits from Role
-public class AppRole : Role
+public class AppRole : Role, IAuditEntity
 {
     public AppRole() { }
     public AppRole(string name) : base(name) { }
@@ -11,4 +13,14 @@ public class AppRole : Role
 
     // Also, Override base properties:
     // public override Guid Id { get; set; }
+
+    public virtual string CreatedBy { get; set; }
+    public virtual string CreatedByIp { get; set; }
+    public virtual DateTime CreatedAt { get; set; }
+
+    public virtual string LastUpdatedBy { get; set; }
+    public virtual string LastUpdatedByIp { get; set; }
+    public virtual DateTime? LastUpdatedAt { get; set; }
+
+    public virtual IApplicationContext Context { get; set; }
 }
--- src/API/Entities/AppUser.cs
+++ src/API/Entities/AppUser.cs
@@ -10,10 +10,14 @@ public class AppUser : User, IAuditEntity
 
     public virtual string FirstName { get; set; }
     public virtual string LastName { get; set; }
+
     public virtual string CreatedBy { get; set; }
     public virtual string CreatedByIp { get; set; }
     public virtual DateTime CreatedAt { get; set; }
+
     public virtual string LastUpdatedBy { get; set; }
     public virtual string LastUpdatedByIp { get; set; }
     public virtual DateTime? LastUpdatedAt { get; set; }
+
+    public virtual IApplicationContext Context { get; set; }
 }
