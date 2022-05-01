c921f2b1ec3f7501d337fa79a598e77c95e398cb
Hesam Asnaashari <46031836+ilhesam@users.noreply.github.com>
2022-05-01 14:45:01 +0430

    Remove audit props from user entity ( unused code )

--- src/API/Entities/AppUser.cs
+++ src/API/Entities/AppUser.cs
@@ -1,23 +1,11 @@
-﻿using System;
-
-namespace API;
+﻿namespace API;
 
 // Inherits from User
-public class AppUser : User, IAuditEntity
+public class AppUser : User
 {
     public AppUser() { }
     public AppUser(string userName) : base(userName) { }
 
     public virtual string FirstName { get; set; }
     public virtual string LastName { get; set; }
-
-    public virtual string CreatedBy { get; set; }
-    public virtual string CreatedByIp { get; set; }
-    public virtual DateTime CreatedAt { get; set; }
-
-    public virtual string LastUpdatedBy { get; set; }
-    public virtual string LastUpdatedByIp { get; set; }
-    public virtual DateTime? LastUpdatedAt { get; set; }
-
-    public virtual IApplicationContext Context { get; set; }
 }
