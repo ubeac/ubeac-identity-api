95e65abae49c6e4967f28e2578afe0067b3751f3
Hesam Asnaashari <46031836+ilhesam@users.noreply.github.com>
2022-05-01 14:44:55 +0430

    Remove audit props from role entity ( unused code )

--- src/API/Entities/AppRole.cs
+++ src/API/Entities/AppRole.cs
@@ -1,9 +1,7 @@
-﻿using System;
-
-namespace API;
+﻿namespace API;
 
 // Inherits from Role
-public class AppRole : Role, IAuditEntity
+public class AppRole : Role
 {
     public AppRole() { }
     public AppRole(string name) : base(name) { }
@@ -13,14 +11,4 @@ public class AppRole : Role, IAuditEntity
 
     // Also, Override base properties:
     // public override Guid Id { get; set; }
-
-    public virtual string CreatedBy { get; set; }
-    public virtual string CreatedByIp { get; set; }
-    public virtual DateTime CreatedAt { get; set; }
-
-    public virtual string LastUpdatedBy { get; set; }
-    public virtual string LastUpdatedByIp { get; set; }
-    public virtual DateTime? LastUpdatedAt { get; set; }
-
-    public virtual IApplicationContext Context { get; set; }
 }
